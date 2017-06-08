694188b8055ec9c36d77b56dd833cc5ea34cbd72
SBI- <pascal.huerlimann@gmail.com>
2017-06-08 16:38:00 +0200

    Add implementation and documentation
    Implement basic functionality for generating readmes with table of
    contents and documented the command line arguments.

--- create.sh
+++ create.sh
@@ -1,10 +1,82 @@
 #!/bin/bash
-# remove hard line breaks to mimik github
 
-# make options for input/output file and make toc optional
+# check for enhanced getopt and quit otherwise
+# any modern linux and CYGWIN will have enhanced getopt
+# however, it's not the default on osx and has to be installed
+getopt --test
+if [[ $? -ne 4 ]]; then
+    echo "Enhanced getopt not found"
+    exit 1
+fi
+
+# set default values
+outfile="readme.html"
+infile="README.md"
+tocDepth=3
+tocOnly=0
+
+# set options for getopt
+shortOps=o:ti:v
+longOps=output:,toc,input:,toc-depth:,verbose,toc-only
+
+options=$(getopt --options $shortOps --longoptions $longOps --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 2
+fi
+eval set -- "$options"
+
+while true; do
+    case "$1" in
+        -o|--output)
+            outfile="$2"
+            shift 2
+            ;;
+        -t|--toc)
+            toc="--toc"
+            shift
+            ;;
+        -i|--input)
+            infile="$2"
+            shift 2
+            ;;
+        --toc-depth)
+            tocDepth="$2"
+            shift 2
+            ;;
+        --toc-only)
+            tocOnly=1
+            echo "TOC only is not implemented yet"
+            shift
+            ;;
+        -v|--verbose)
+            printOptions=1
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Unknown implementation error. This shouldn't happen."
+            exit 3
+            ;;
+    esac
+done
+
+if [[ $printOptions -eq 1 ]]; then
+    echo outfile = $outfile
+    echo infile = $infile
+    echo tocDepth = $tocDepth
+    echo tocOnly = $tocOnly
+    echo toc = $toc
+fi
+
+# remove hard line breaks to mimik github
 pandoc \
-    -o $1 \
+    -o $outfile \
     -f markdown_github-hard_line_breaks \
     --css github.css \
-    --toc \
-    README.md
+    --self-contained \
+    ${toc:+"$toc"} \
+    --toc-depth=$tocDepth \
+    $infile
